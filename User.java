82a3a0541886129032482dfec748fb94741cd1d4
axxxiomatic <pinkod.miet16@gmail.com>
2024-10-23 01:04:47 +0300

    test commit

---
+++ User.java
@@ -0,0 +1,14 @@
+public class User {
+    private int userNumber;
+    private String userLogin;
+    private String userPassword;
+    private int[] userWishes;
+    private String userPhoneNumber;
+
+    User(int myNumber, String myLogin, String myPassword, String myPhoneNumber) {
+        self.userNumber = myNumber;
+        self.userLogin = myLogin;
+        self.userPassword = myPassword;
+        self.userPhoneNumber = myPhoneNumber;
+    }
+}
